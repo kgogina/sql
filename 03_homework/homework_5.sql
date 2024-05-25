9db9144ea2994ae443caa26719b1a7bf898b4b40
Kateryna Gogina <k.gogina@gmail.com>
2024-05-24 22:19:52 -0400

    homework_5

--- 03_homework/homework_5.sql
+++ 03_homework/homework_5.sql
@@ -1,32 +1,3 @@
--- String manipulations
-/* 1. Some product names in the product table have descriptions like "Jar" or "Organic". 
-These are separated from the product name with a hyphen. 
-Create a column using SUBSTR (and a couple of other commands) that captures these, but is otherwise NULL. 
-Remove any trailing or leading whitespaces. Don't just use a case statement for each product! 
-
-| product_name               | description |
-|----------------------------|-------------|
-| Habanero Peppers - Organic | Organic     |
-
-Hint: you might need to use INSTR(product_name,'-') to find the hyphens. INSTR will help split the column. */
-
-
-
-/* 2. Filter the query to show any product_size value that contain a number with REGEXP. */
-
-
-
--- UNION
-/* 1. Using a UNION, write a query that displays the market dates with the highest and lowest total sales.
-
-HINT: There are a possibly a few ways to do this query, but if you're struggling, try the following: 
-1) Create a CTE/Temp Table to find sales values grouped dates; 
-2) Create another CTE/Temp table with a rank windowed function on the previous query to create 
-"best day" and "worst day"; 
-3) Query the second temp table twice, once for the best day, once for the worst day, 
-with a UNION binding them. */
-
-
 
 -- Cross Join
 /*1. Suppose every vendor in the `vendor_inventory` table had 5 of each of their products to sell to **every** 
@@ -39,6 +10,28 @@ Think a bit about the row counts: how many distinct vendors, product names are t
 How many customers are there (y). 
 Before your final group by you should have the product of those two queries (x*y).  */
 
+SELECT 
+	vendor_name,
+	product_name,
+	original_price * 5 * number_of_customers as earning_per_product 
+FROM (
+	SELECT DISTINCT 
+		vendor_id,
+		product_id,
+		original_price,
+		number_of_customers
+	FROM
+		vendor_inventory as vi
+	CROSS JOIN (
+		SELECT 
+		count(DISTINCT (customer_id)) AS number_of_customers
+		FROM customer ) as nc
+) as a
+
+INNER JOIN product as p
+ON a.product_id = p.product_id
+INNER JOIN vendor as v
+ON v.vendor_id = a.vendor_id
 
 
 -- INSERT
@@ -47,11 +40,33 @@ This table will contain only products where the `product_qty_type = 'unit'`.
 It should use all of the columns from the product table, as well as a new column for the `CURRENT_TIMESTAMP`.  
 Name the timestamp column `snapshot_timestamp`. */
 
+DROP TABLE IF EXISTS product_units;
+CREATE TABLE IF NOT EXISTS product_units AS
+SELECT 
+    product_id,
+    product_name,
+    product_size,
+    product_category_id,
+    product_qty_type,
+    CURRENT_TIMESTAMP AS snapshot_timestamp
+FROM 
+    product
+WHERE 
+    product_qty_type = 'unit'
 
 
 /*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
 This can be any product you desire (e.g. add another record for Apple Pie). */
 
+INSERT INTO product_units
+VALUES(
+	(SELECT product_id FROM product WHERE lower(product_name) = 'apple pie'),
+	'Apple Pie',
+	'10"',
+	(SELECT product_category_id FROM product_category WHERE lower(product_category_name) = 'packaged prepared food'),
+	'unit',
+	CURRENT_TIMESTAMP
+	)
 
 
 -- DELETE
@@ -59,6 +74,12 @@ This can be any product you desire (e.g. add another record for Apple Pie). */
 
 HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
 
+DELETE from product_units
+WHERE product_name = 'Apple Pie'
+	AND snapshot_timestamp = (
+		SELECT MIN(snapshot_timestamp)
+		FROM product_units
+		WHERE product_name = 'Apple Pie')
 
 
 -- UPDATE
@@ -79,3 +100,11 @@ Finally, make sure you have a WHERE statement to update the right row,
 When you have all of these components, you can run the update statement. */
 
 
+UPDATE product_units
+SET current_quantity = COALESCE((
+        SELECT quantity
+        FROM vendor_inventory
+        WHERE product_id = product_units.product_id
+        ORDER BY market_date DESC
+        LIMIT 1
+    ), 0)
